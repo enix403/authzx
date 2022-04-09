0d60538e1278a27d02bf75901373dd7e842f2ed1
enix403 <fog.code000@gmail.com>
2022-04-09 23:48:12 +0500

    Updated setup.py

--- setup.py
+++ setup.py
@@ -11,9 +11,9 @@ setuptools.setup(
     description="A library to manage authorization",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/enix403/authzx",
     project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+        "Bug Tracker": "https://github.com/enix403/authzx/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
